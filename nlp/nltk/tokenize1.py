cb78be4b1128527e4a654997200f36feafb6dbf5
biobots <biobot@qq.com>
2019-04-10 16:40:54 +0800

    tokenize

--- nlp/nltk/tokenize1.py
+++ nlp/nltk/tokenize1.py
@@ -3,17 +3,20 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
 
-res = request.urlopen("http://php.net")
-html = res.read()
-soup = BeautifulSoup(html, "html5lib")
-text = soup.get_text(strip=True)
-tokens = text.split()
-clean_tokens = list()
-sr = stopwords.words("english")
-for token in tokens:
-    if token not in sr:
-        clean_tokens.append(token)
-freq = nltk.FreqDist(clean_tokens)
-for key,value in freq.items():
-    print(str(key)+":"+str(value))
-freq.plot(20, cumulative=False)
+def tkn1():
+    res = request.urlopen("http://php.net")
+    html = res.read()
+    soup = BeautifulSoup(html, "html5lib")
+    text = soup.get_text(strip=True)
+    tokens = text.split()
+    clean_tokens = list()
+    sr = stopwords.words("english")
+    for token in tokens:
+        if token not in sr:
+            clean_tokens.append(token)
+    freq = nltk.FreqDist(clean_tokens)
+    for key,value in freq.items():
+        print(str(key)+":"+str(value))
+    #freq.plot(20, cumulative=False)
+    return clean_tokens
+
--- nlp/nltk/tokenize2.py
+++ nlp/nltk/tokenize2.py
@@ -1,5 +1,32 @@
-from nltk.tokenize import sent_tokenize
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
 
-test = "Hello Adam, how are you? I hope everything is going well. Today is a good day, see you dude."
+test = "Hello Mr.Adam, how are you? I hope everything is going well. Today is a good day, see you dude."
 
 print(sent_tokenize(test))
+print(word_tokenize(test))
+
+
+from nltk.corpus import wordnet
+
+syn = wordnet.synsets("depress")
+print(syn[0].definition())
+print(syn[0].examples())
+
+
+from nltk.tokenize import WhitespaceTokenizer
+
+t = WhitespaceTokenizer().tokenize(test)
+print(t)
+
+
+from tokenize1 import tkn1
+
+tagged = nltk.pos_tag(tkn1(), tagset='universal', lang='eng')
+tagged_verb = list()
+for key,val in tagged:
+    if val == 'VERB':
+        tagged_verb.append((key,val))
+print(tagged_verb)
+text_fd = nltk.FreqDist(val for (key, val) in tagged)
+print(text_fd.most_common())
