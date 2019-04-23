0a993ee7423a38ecbfc51e352e697b1cc7715755
biobots <biobot@qq.com>
2019-04-23 19:50:36 +0800

    add shader manager

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -1,9 +1,15 @@
     #include <glad/glad.h>
     #include <GLFW/glfw3.h>
     #include <iostream>
+    #include "shader.h"
     // settings
     const unsigned int SCR_WIDTH = 800;
     const unsigned int SCR_HEIGHT = 600;
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+     };
     int main()
     {
         // glfw: initialize and configure
@@ -32,12 +38,31 @@
             std::cout << "Failed to initialize GLAD" << std::endl;
             return -1;
         }
+
+        unsigned int VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        unsigned int VAO;
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        Shader shader("src\\shaders\\test.vs", "src\\shaders\\test.fs");
+
         // render loop
         // -----------
         while (!glfwWindowShouldClose(window))
         {
             glClearColor(0.0f, 1.f, 0.0f,1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+            shader.use();
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
             // glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
             // ---------------------------------------------------
             glfwSwapBuffers(window);
