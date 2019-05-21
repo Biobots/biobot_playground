29df8d808b7f346fe004f3b602779ad78498601c
biobots <biobot@qq.com>
2019-05-21 12:18:54 +0800

    digital defog: cal dark channel

--- work/defog/src/main.cpp
+++ work/defog/src/main.cpp
@@ -10,23 +10,58 @@ using namespace std;
 int main()
 {
     Mat img = imread("b.jpg", ImreadModes::IMREAD_COLOR);
-	imshow("compile test", img);
+	imshow("original", img);
     int channels = img.channels();
     int r, g, b;
+    Mat tmp(img.size(), img.type());
     for (int i = 0; i < img.rows; i++)
     {
         uchar* p = img.ptr<uchar>(i);
+        uchar* output = tmp.ptr<uchar>(i);
         for (int j = 0; j < img.cols * channels; j += channels)
         {
-            b += (int)p[j];
-            g += (int)p[j+1];
-            r += (int)p[j+2];
+            uchar* tmp = std::min(&p[j], &p[j + 1]);
+            tmp = std::min(tmp, &p[j + 2]);
+            output[j] = *tmp;
+            output[j + 1] = *tmp;
+            output[j + 2] = *tmp;
         }
     }
-    r /= img.rows * img.cols;
-    g /= img.rows * img.cols;
-    b /= img.rows * img.cols;
-    printf("r:%d, g:%d, b:%d", r, g, b);
+    Mat drk(img.size(), CV_8UC1);
+    cvtColor(tmp, drk, COLOR_BGR2GRAY);
+    imshow("step 1", drk);
+    int radius = 7;
+    Mat drkc(img.size(), CV_8UC1);
+    for (int i = radius; i < drk.rows; i += 2 * radius + 1)
+    {
+        
+        for (int j = radius; j < drk.cols; j += 2 * radius + 1)
+        {
+            uchar minnum;
+            minnum = drk.ptr<uchar>(i)[j];
+            for (int h = -radius; h < 2 * radius + 1; h++)
+            {
+                for (int k = - radius; k < 2 * radius + 1; k++)
+                {
+                    if (i + h < drk.rows && j + k < drk.cols)
+                    {
+                        minnum = *std::min(&minnum, &drk.ptr<uchar>(i + h)[j + k]);
+                    }
+                }
+            }
+            for (int h = -radius; h < 2 * radius + 1; h++)
+            {
+                for (int k = -radius; k < 2 * radius + 1; k++)
+                {
+                    if (i + h < drk.rows && j + k < drk.cols)
+                    {
+                        drkc.ptr<uchar>(i + h)[j + k] = minnum;
+                    }
+                }
+            }
+        }
+    }
+    imshow("step 2", drkc);
     waitKey();
 	return 0;
 }
