bf9370ead08799b9a22c1c99dda789ab1c3aeedb
Biobots <biobot@qq.com>
2019-06-20 21:09:04 +0800

    sync

---
+++ python100/tf/test1.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+m1 = tf.constant([3, 5])
+m2 = tf.constant([2, 4])
+result = tf.add(m1, m2)
+print(result)
--- work/guide_filter/src/main.cpp
+++ work/guide_filter/src/main.cpp
@@ -10,24 +10,106 @@ using namespace std;
 
 Mat guidedfilter(Mat &srcImage, Mat &srcClone, int r, double eps);
 
+int* CalRowSum(uchar* data, int* rst, int radius, int width)
+{
+    //expand row
+    uchar rowdata[width + 2 * radius];
+    int i;
+    int size = 2 * radius + 1;
+    for (i = 0; i < radius; i ++)
+    {
+        rowdata[i] = *data;
+    }
+    for (i = 0; i < radius; i ++)
+    {
+        rowdata[i + width] = *(data + width - 1);
+    }
+    memcpy(rowdata + radius, data, width * sizeof(uchar));
+    //calculate sum
+    int tmp = 0;
+    for (i = 0; i < size; i++)
+    {
+        tmp += rowdata[i];
+    }
+    *(rst) = tmp;
+    for (i = 1; i < width; i++)
+    {
+        tmp += rowdata[i + size - 1] - rowdata[i - 1];
+        *(rst + i) = tmp;
+    }
+    return rst;
+}
+
+uchar* myBoxFilter(uchar* src, uchar* rst, int radius, int width, int height)
+{
+    //calculate row sum
+    int rowsum[width * height];
+    int size = 2 * radius + 1;
+    float scale = 1.0f / (float)(size * size);
+    int* colpos[height + 2 * radius];
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < height; i++)
+    {
+        CalRowSum(src + width * i, rowsum + width * i, radius, width);
+        colpos[i + radius] = rowsum + width * i;
+    }
+    for (i = 1; i <= radius; i++)
+    {
+        colpos[radius - i] = colpos[radius];
+        colpos[height + radius + i - 1] = colpos[height + radius - 1];
+    }
+    //calculate first column
+    int colsum[width];
+    memset(colsum, 0, width * sizeof(int));
+    for (i = 0; i < size - 1; i++)
+    {
+        int* rowhead = colpos[i];
+        for (j = 0; j < width; j++)
+        {
+            colsum[j] += rowhead[j];
+        }
+    }
+    //calbulate column sum by row
+    int val = 0;
+    for (i = 0; i < height; i++)
+    {
+        uchar* rstrow = rst + width * i;
+        int* addpos = colpos[i + size - 1];
+        int* subpos = colpos[i];
+        for (j = 0; j < width; j++)
+        {
+            val = colsum[j] + addpos[j];
+            rstrow[j] = (uchar)(val * scale);
+            colsum[j] = val - subpos[j];
+        }
+    }
+    return rst;
+}
+
 int main()
 {
-    Mat src = imread("c.jpg", ImreadModes::IMREAD_COLOR);
-    vector<Mat> vsrc, vrst;
-    split(src, vsrc);
-    Mat rst;
-    for (int i = 0; i < src.channels(); i++)
+    Mat gray = imread("d.jpg", ImreadModes::IMREAD_GRAYSCALE);
+    //Mat gray(src.rows,src.cols,CV_8UC1,src);
+    gray.convertTo(gray, CV_8UC1);
+    imshow("original", gray);
+    uchar* img = (uchar*)malloc(gray.rows * gray.cols * sizeof(uchar));
+    for (int i = 0; i < gray.rows; i++)
     {
-        Mat tmp;
-        vsrc[i].convertTo(tmp, CV_64FC1, 1.0/255.0);
-        Mat p = tmp.clone();
-        Mat result = guidedfilter(tmp, p, 6, 0.001);
-        result.convertTo(result, CV_8UC1, 255.0);
-        vrst.push_back(result);
+        for (int j = 0; j < gray.cols; j++)
+        {
+            uchar tmp = gray.at<uchar>(i, j);
+            img[j + i * gray.cols] = tmp;
+        }
     }
-    merge(vrst, rst);
-    imshow("origina", src);
-    imshow("filter", rst);
+    uchar* rst = (uchar*)malloc(gray.rows * gray.cols * sizeof(uchar));
+    Mat f;
+    rst = myBoxFilter(img, rst, 1, gray.cols, gray.rows);
+    //boxFilter(gray, f, CV_8UC1, Size(1, 1));
+    f = cv::Mat(gray.rows, gray.cols, CV_8UC1, rst);
+    imshow("aft", f);
+    free(img);
+    free(rst);
     waitKey(0);
     return 0;
 }
