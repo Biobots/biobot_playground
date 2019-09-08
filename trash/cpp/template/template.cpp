a2331d1efba7fbcc6aacae70cacb92cc031ce8e4
Biobots <biobot@qq.com>
2019-09-08 21:37:41 +0800

    just sync

--- trash/cpp/template/template.cpp
+++ trash/cpp/template/template.cpp
@@ -1,5 +1,45 @@
 #include <future>
 
+//decide return type
+template<bool, typename T = void>
+struct enable_if {};
+
+template<typename T>
+struct enable_if<true, T>
+{
+	typedef T Type;
+};
+
+template<typename T>
+struct is_integer
+{
+	bool val = false;
+};
+
+template<>
+struct is_integer<unsigned char>
+{
+	bool val = true;
+};
+
+template<>
+struct is_integer<char>
+{
+	bool val = true;
+};
+
+template<>
+struct is_integer<int>
+{
+	bool val = true;
+};
+
+template<>
+struct is_integer<unsigned int>
+{
+	bool val = true;
+};
+
 int main()
 {
 	return 0;
