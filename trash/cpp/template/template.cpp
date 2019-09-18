83295d7493a0a6a142336141bb2b15841ef6629c
biobots <biobot@qq.com>
2019-09-18 18:40:15 +0800

    type traits

--- trash/cpp/template/template.cpp
+++ trash/cpp/template/template.cpp
@@ -1,46 +1,80 @@
 #include <future>
+#include <iostream>
 
-//decide return type
-template<bool, typename T = void>
+template<typename T, T v>
+struct integral_constant
+{
+    static constexpr T value = v;
+    typedef T value_type;
+    typedef integral_constant type;
+};
+
+typedef integral_constant<int, 2> two_type;
+typedef integral_constant<int, 6> six_type;
+
+typedef integral_constant<bool, true> true_type;
+typedef integral_constant<bool, false> false_type;
+
+template<bool b, class T = void>
 struct enable_if {};
 
-template<typename T>
+template<class T>
 struct enable_if<true, T>
 {
-	typedef T Type;
+    typedef T type;
 };
+
+static_assert(two_type::value * 3 == six_type::value, "err");
+
+template<class T>
+struct is_const : public false_type {};
+
+template<class T>
+struct is_const<const T> : public true_type {};
+
+template<class ...T> struct common_type;
+
+template<class T>
+struct common_type<T>
+{
+    //std::decay返回退化后的基本类型
+    typedef typename std::decay<T>::type type;
+};
+
+template<class T, class U>
+struct common_type<T, U>
+{
+private:
+    static T&& t();
+    static U&& u();
+    static bool f();
+public:
+    typedef typename std::decay<decltype(f() ? t() : u())>::type type;
+};
+
+template<class T, class U, class ...V>
+struct common_type<T, U, V...>
+{
+    typedef typename common_type<typename common_type<T, U>::type, V...>::type type;
+};
+
+template<typename T, typename U = std::string>
+struct has_to_string : false_type {};
 
 template<typename T>
-struct is_integer
-{
-	bool val = false;
+struct has_to_string<T, decltype(std::declval<T>().to_string())> : true_type {};
+
+struct A {
+    std::string to_string();
 };
 
-template<>
-struct is_integer<unsigned char>
-{
-	bool val = true;
-};
+struct B {
 
-template<>
-struct is_integer<char>
-{
-	bool val = true;
-};
-
-template<>
-struct is_integer<int>
-{
-	bool val = true;
-};
-
-template<>
-struct is_integer<unsigned int>
-{
-	bool val = true;
 };
 
 int main()
 {
+    std::cout << has_to_string<A>::value << std::endl;
+    std::cout << has_to_string<B>::value << std::endl;
 	return 0;
 }
